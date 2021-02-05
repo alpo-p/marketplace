9eca737a1b6b8fc3a5ef60c0f04a161dd2db5868
supea <alpo.panula@gmail.com>
2021-02-05 12:43:23 +0200

    Ability to add your ads

--- routes.py
+++ routes.py
@@ -141,8 +141,29 @@ def profile():
     # TODO!!
     return render_template("profile.html")
 
-@app.route("/editads")
-def editads():
-    # TODO
-    # Sivu, jolla voit lisätä ja muokata ilmoituksiasi
-    return render_template("editads.html")
+@app.route("/my_ads")
+def edit_ads():
+    return render_template("my_ads.html")
+
+@app.route("/make_ad", methods=["POST"])
+def make_ad():
+    name = request.form["name"]
+    category = request.form["category"]
+    ostetaan = request.form["ostetaan"]
+    kuvaus = request.form["kuvaus"]
+    hinta = request.form["hinta"]
+
+    sql_a = "INSERT INTO items (category_id, name, date_added, visible)" \
+            "VALUES (:category, :name, NOW(), TRUE) RETURNING id"
+    result_a = db.session.execute(sql_a, {"category":category,"name":name})
+    db.session.commit()
+    item_id = result_a.fetchone()[0]
+
+    sql_b = "INSERT INTO iteminfo (item_id, user_id, price, kuvaus, ostetaan)" \
+            "VALUES (:item_id, :user_id, :price, :kuvaus, :ostetaan)"
+    sql_c = "SELECT id FROM users WHERE username=:name"
+    user_id = db.session.execute(sql_c, {"name":session["username"]}).fetchone()[0]
+    db.session.execute(sql_b, {"item_id":item_id,"user_id":user_id,"price":hinta,"kuvaus":kuvaus,"ostetaan":ostetaan})
+    db.session.commit()
+
+    return redirect("/my_ads")
