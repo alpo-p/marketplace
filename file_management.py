b7f0e273447b139c4f9da65e36ad019a1b6170be
supea <alpo.panula@gmail.com>
2021-02-15 10:27:27 +0200

    heroku changes

--- file_management.py
+++ file_management.py
@@ -13,11 +13,9 @@ def upload_file(path, file_name, bucket):
     s3.meta.client.upload_file(f"{path}/{file_name}", bucket, file_name)
 
 def upload_picture(picture):
-    # random pic_name
     pic_name = uuid.uuid4().hex + picture.filename
     pic_path = path.join(UPLOAD_FOLDER, pic_name) 
     picture.save(pic_path)
     upload_file(UPLOAD_FOLDER, pic_name, bucket)
-    # remove the temp file
     remove(pic_path)
     return pic_name
