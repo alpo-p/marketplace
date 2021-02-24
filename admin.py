5514379410baaf6b2233e229b49a4a6915ee2847
supea <alpo.panula@gmail.com>
2021-02-24 14:19:15 +0200

    CSRF attack protection

--- admin.py
+++ admin.py
@@ -2,6 +2,7 @@ from app import app
 from flask import render_template, redirect, request, session
 from werkzeug.security import check_password_hash
 from db import db
+from os import urandom
 
 @app.route("/admin")
 def admin():
@@ -15,6 +16,7 @@ def admin_login():
     result = db.session.execute(sql, {"username":username})
     admin = result.fetchone()
     if admin and check_password_hash(admin[0],password):
+        session["admin_token"] = urandom(16).hex()
         session["admin"] = username
     else:
         print("ERROR")
@@ -31,6 +33,8 @@ def new_category():
         session["admin"]
     except:
         return redirect("/")
+    if session["admin_token"] != request.form["admin_token"]: 
+        return False
     new = request.form["name_of_category"]
     sql = "INSERT INTO categories (name) VALUES (:new)"
     db.session.execute(sql, {"new":new})
